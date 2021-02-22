ec9571c0a2f92c5d241c1afaa19baad9e570244c
mateusztront <mateusztront@gmail.com>
2021-02-22 12:40:45 +0100

    dodanie MainView

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -9,3 +9,8 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+class MainView(View):
+
+    def get(self, request):
+        return render(request, "index.html")
