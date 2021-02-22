70b0a8bfd1a85f145b6fb7725025a9d8a0628b7a
Raekker <maciej.purta@wp.pl>
2021-02-22 13:37:06 +0100

    add views and urls

--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -16,13 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
-from jedzonko.views import IndexView, DashboardView, MainView
-
+from jedzonko.views import (IndexView, DashboardView, MainView, RecipeDetailView, RecipeListView, RecipeAddView
+                            , RecipeModifyView, PlanDetailView, PlanListView, PlanAddView, PlanAddRecipeView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view()),
-    path('', MainView.as_view())
+    path('', MainView.as_view()),
     path('main/', DashboardView.as_view(), name='main'),
+    path('recipe/<int:id>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/list/', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/add/', RecipeAddView.as_view(), name='recipe_add'),
+    path('recipe/modify/<int:id>/', RecipeModifyView.as_view(), name='recipe_modify'),
+    path('plan/<int:id>/', PlanDetailView.as_view(), name='plan_detail'),
+    path('plan/list/', PlanListView.as_view(), name='plan_list'),
+    path('plan/add/', PlanAddView.as_view(), name='plan_add'),
+    path('plan/add-recipe/', PlanAddRecipeView.as_view(), name='plan_add_recipe'),
 ]
