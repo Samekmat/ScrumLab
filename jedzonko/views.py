3e22ea6af7009095655a85d0ae069a93bad46801
mateusztront <mateusztront@gmail.com>
2021-02-25 15:09:25 +0100

    porzadek w views

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -117,11 +117,8 @@ class RecipeModifyView(View):
 class PlanDetailView(View):
     def get(self, request, id):
         plan = Plan.objects.get(pk=id)
-        recipeplan_all_day_name = [x.day_name for x in plan.recipeplan_set.all()]
-        days = set(recipeplan_all_day_name)
-        recipeplan = Recipeplan.objects.filter(plan_id=id)
         ctx = {
-            "plan": plan, "days": days, "recipeplan": recipeplan,
+            "plan": plan,
         }
         return render(request, "app-details-schedules.html", ctx)
 
